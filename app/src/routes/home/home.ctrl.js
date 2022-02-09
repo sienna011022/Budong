8f4319bdeff5db4106e3ae3d7ff80dca729f077c
sienna <20200803@sungshin.ac,kr>
2022-02-10 08:59:23 +0900

    로그인 화면 오픈소스로 꾸미기

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const UserStorage = require("../../models/UserStorage");
-
+const User = require("../../models/User");
 const output = {
     hello : (req,res) => {
         res.render("home/index");
@@ -13,21 +13,25 @@ const output = {
 
 const process = {
     login : (req,res) => {
-        const id = req.body.id,
-        psword = req.body.psword;
-        //로그인 인증 여부
-       const users = UserStorage.getUsers("id","psword");
-        const response = {};
-        if(users.id.includes(id)) {
-            const idx = users.id.indexOf(id);
-            if(users.psword[idx] == psword){
-                response.success = true;
-                return res.json(response);
-            }
-        }
-        response.success = false;
-        response.msg = "로그인에 실패하셨습니다";
+        const user = new User(req.body);
+        const response = user.login();
         return res.json(response);
+        // return res.json(response);
+    //     const id = req.body.id,
+    //     psword = req.body.psword;
+    //     //로그인 인증 여부
+    //    const users = UserStorage.getUsers("id","psword");
+    //     const response = {};
+    //     if(users.id.includes(id)) {
+    //         const idx = users.id.indexOf(id);
+    //         if(users.psword[idx] == psword){
+    //             response.success = true;
+    //             return res.json(response);
+    //         }
+    //     }
+    //     response.success = false;
+    //     response.msg = "로그인에 실패하셨습니다";
+    //     return res.json(response);
    
      },
 
