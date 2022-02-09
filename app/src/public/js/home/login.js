c5a95f8e77ebba58660459593d43ea5499db919f
sienna <20200803@sungshin.ac,kr>
2022-02-09 12:20:38 +0900

    login을 요청하는 fetch 생성

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -8,9 +8,15 @@ loginBtn.addEventListener("click",login);
 function login(){
     const req = {
         id : id.value,
-        pasword : psword.value,
+        psword : psword.value,
     
     };
     
-    console.log(req);
+    fetch("/login",{
+        method : "POST",
+        header :{
+            "Content-Type" : "application/json"
+        },
+        body : JSON.stringify(req)
+    })
 }
