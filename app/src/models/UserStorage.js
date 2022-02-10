b18beed27c73c321a635127eb882fae1d74c3c30
sienna <20200803@sungshin.ac,kr>
2022-02-10 16:05:01 +0900

    파일시스템을 통한 DB

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -1,42 +1,46 @@
 "use strict";
+
+const fs = require("fs").promises;
 class UserStorage{
-    //은닉화 처리 #
-   static #users = {
-    id : ["sienna","dong"],
-    psword : ["1022","0320"],
-    name : ["김성윤","김동현"]
-};
-//받아올 수 있도록 매소드 설정
-static getUsers(...fields) { 
-   const users = this.#users;
-   const newUsers = fields.reduce((newUsers,field) => {
-       if(users.hasOwnProperty(field)){
-           newUsers[field] = users[field];}
-           return newUsers;
-       },{});
-       return newUsers;
-        
-}
-    static getUserInfo(id){
-        const users = this.#users;
-        const idx = users.id.indexOf(id);
-        const userKeys = Object.keys(users);
-        const userInfo = userKeys.reduce((newUser,info) => {
-            newUser[info] = users[info][idx];
-            return newUser;
+    static #getUserInfo(data,id){
+            const users= JSON.parse(data);
+             // const users = this.#users;
+             const idx = users.id.indexOf(id);
+             const userKeys = Object.keys(users);
+             const userInfo = userKeys.reduce((newUser,info) => {
+                 newUser[info] = users[info][idx];
+                 return newUser;
+             },{});
+             return userInfo;  
+            }
+
+    static getUsers(...fields) { 
+    //    const users = this.#users;
+    const newUsers = fields.reduce((newUsers,field) => {
+        if(users.hasOwnProperty(field)){
+            newUsers[field] = users[field];}
+            return newUsers;
         },{});
-        return userInfo;
+        return newUser;
+            
+    }
+    static getUserInfo(id){
+       return fs
+       .readFile("./src/databases/users.json")
+         .then((data) => {
+             return this.#getUserInfo(data,id);
+         })
+         .catch(console.error);
         }
-
-    
-
+        
+   
     static save(userInfo){
-        const users = this.#users;
+        // const users = this.#users;
         users.id.push(userInfo.id);
         users.name.push(userInfo.name);
         users.psword.push(userInfo.psword);
         return {success : true};
     
     }
-}
-module.exports = UserStorage;
+    }
+    module.exports = UserStorage;
